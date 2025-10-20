b5f24578126dbe2958ffe19021e863103566a06d
Luka <lukagaxokidze28@gmail.com>
2025-10-21 00:54:23 +0400

    update: responsive layout for posts page, partially users page

--- src/app/shared/components/layout/navigation/navigation.component.ts
+++ src/app/shared/components/layout/navigation/navigation.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { NAVIGATION_ITEMS } from '../../../consts/navigation.constant';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink, RouterLinkActive, RouterModule],
+  imports: [RouterLink, RouterLinkActive, RouterModule, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
   NAV_ITEMS = NAVIGATION_ITEMS;
+  isOpen = signal<boolean>(false);
 }
