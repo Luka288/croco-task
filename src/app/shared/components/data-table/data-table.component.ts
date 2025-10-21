5ce20ca6126e9e2b8e6ea6881bd044916391907c
Luka <lukagaxokidze28@gmail.com>
2025-10-21 13:53:55 +0400

    update: users page responsive layout

--- src/app/shared/components/data-table/data-table.component.ts
+++ src/app/shared/components/data-table/data-table.component.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-data-table',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatButton],
   templateUrl: './data-table.component.html',
   styleUrl: './data-table.component.scss',
 })
-export class DataTableComponent<T> {
+export class DataTableComponent<T> implements OnInit {
   @Input() columns: { header: string; value: (row: T) => string | number }[] =
     [];
   @Input() data: T[] = [];
   @Input() rowActions: { label: string; action: (row: T) => void }[] = [];
+
+  displayedColumns: string[] = [];
+
+  ngOnInit(): void {
+    this.displayedColumns = this.columns.map((col) => col.header);
+
+    if (this.rowActions.length) {
+      this.displayedColumns.push('actions');
+    }
+  }
 }
